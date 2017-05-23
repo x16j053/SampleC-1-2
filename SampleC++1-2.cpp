e4077aa97d3cac988ba9168bb05296dcb59f54e8
WIN_NT\x16j053 <x16j053@chiba-fjb.ac.jp>
2017-05-23 12:28:05 +0900

    名前空間

---
+++ SampleC++1-2.cpp
@@ -0,0 +1,13 @@
+// SampleC++1-2.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+
+int main()
+{
+	std::cout << "HelloWorld" << std::endl;
+    return 0;
+}
+
